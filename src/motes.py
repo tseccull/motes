9b5d6ae7e8d4d68877c43888e9091b4e407d8e82
DAKiersz <59928295+DAKiersz@users.noreply.github.com>
2024-02-26 20:28:32 +0000

    pyproject changes

--- src/motes.py
+++ src/motes.py
@@ -2,10 +2,6 @@
 MOTES Modular and Optimal Tracer and Extractor of Spectra.
 Description: Modular and Optimal Tracer and Extractor of Specrtra (MOTES). A Python package for 
 extracting spectrum from astronomical 2D spectrograms.
-Version: 0.4.5
-Date: 2023-12-23
-Authors: Tom Seccull, Dominik Kiersz
-Licence: GNU General Public License v3.0
 """
 
 import copy
